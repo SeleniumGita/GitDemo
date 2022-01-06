a26dcf487590da45de848da4939420fab7356809
SeleniumGita <catchgeets@gmail.com>
2022-01-06 14:42:40 -0600

    commit thru old eclipse

---
+++ src/test/java/excercisepkg/TestCase1.java
@@ -0,0 +1,10 @@
+package excercisepkg;
+
+import org.testng.annotations.Test;
+public class TestCase1 {
+  @Test
+  public void hello() {
+	  System.out.println("Inside test1 method");
+	   }
+  
+}
---
+++ src/test/java/excercisepkg/TestCase2.java
@@ -0,0 +1,16 @@
+package excercisepkg;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.Reporter;
+import org.testng.annotations.AfterTest;
+
+public class TestCase2 {
+  
+  @Test
+  public void beforeTest() {
+	  System.out.println("Inside test2 method");
+	 
+	 }
+
+ }
